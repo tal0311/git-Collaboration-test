c186efcd92a610f8904838980b0d58b269bd2197
Guy <orcabasic@gmail.com>
2022-02-23 16:18:34 +0200

    added storage file

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,32 @@
 export const locService = {
-    getLocs
-}
+    getLocs,
+    createNewLoc,
+};
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+];
+
+function createNewLoc(id, name, lat, lng, weather, createdAt, updatedAt) {
+    return {
+        id,
+        name,
+        lat,
+        lng,
+        weather,
+        createdAt,
+        updatedAt,
+    };
+}
+
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(locs);
-        }, 2000)
+        }, 2000);
     });
 }
-
 
